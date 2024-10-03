a0412aa5d901c3e8f54d9aa794894ffb7b07425a
usamarazzaq90 <usamarazzaq9014@gmail.com>
2024-10-03 07:39:26 +0500

    completed part 4

--- 4- Express JS/script.js
+++ 4- Express JS/script.js
@@ -1,4 +1,36 @@
 const express=require('express');
 const app=express();
 
-// Creating routes
+// 1 way of writing middlewares
+app.use(function(req, res, next){
+    console.log("Middleware running 1");
+    next();  
+});
+
+app.use(function(req, res, next){
+    console.log("Middleware running 2 ");
+    next();  
+});
+
+// Creating routes  (e.g., facebook.com/profile)
+app.get("/", function(req,res){
+    res.send("champion mer chota anuj")
+})
+
+app.get("/about", function(req,res){
+    res.send("about page")
+})
+
+
+app.get("/profile", function(req,res, next){
+    return next(new Error("something wrong"));
+    // res.send("profile page")
+})
+
+//Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("Something went wrong, we don't have any idea!!")
+  })
+  
+app.listen(3000);
